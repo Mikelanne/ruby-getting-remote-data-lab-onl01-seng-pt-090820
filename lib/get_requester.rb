54877759892e5ed8d44f2c52d0b8ac22b2a285e5
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-16 20:43:21 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,15 +1,18 @@
 # Write your code here
 require 'open-uri'
 require 'net/http'
+require 'pry'
 
 class GetRequester
   
  def initialize(url)
-   @url = url
+  @url = url
  end 
  
  def get_response_body
-    response =  NET::HTTP.get_response(self)
-    response.body
+   binding.pry
+  uri = URI.parse(self.url)
+  response =  NET::HTTP.get_response(self)
+  response.body
  end 
 end
